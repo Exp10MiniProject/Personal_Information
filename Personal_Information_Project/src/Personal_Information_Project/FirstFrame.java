cf1742c5f9d74cb2181947b953df0fa1318c2b04
nates <nates@192.168.1.8>
2021-05-17 01:01:40 +0530

    Created Dumb Table for entry

--- Personal_Information_Project/src/Personal_Information_Project/FirstFrame.java
+++ Personal_Information_Project/src/Personal_Information_Project/FirstFrame.java
@@ -18,6 +18,7 @@ import java.awt.SystemColor;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JFormattedTextField;
 //hi frm sanjana
 public class FirstFrame extends JFrame {
 
@@ -62,144 +63,145 @@ public class FirstFrame extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel Enter_label = new JLabel("Enter Details");
+		Enter_label.setBounds(10, 11, 582, 20);
 		Enter_label.setHorizontalAlignment(SwingConstants.CENTER);
 		Enter_label.setFont(new Font("Lucida Sans", Font.BOLD | Font.ITALIC, 17));
-		Enter_label.setBounds(10, 11, 582, 20);
 		contentPane.add(Enter_label);
 		
 		JLabel Name = new JLabel("Name");
-		Name.setFont(new Font("Arial", Font.BOLD, 15));
 		Name.setBounds(10, 39, 116, 38);
+		Name.setFont(new Font("Arial", Font.BOLD, 15));
 		contentPane.add(Name);
 		
 		JLabel dob = new JLabel("Date of birth");
-		dob.setFont(new Font("Arial", Font.BOLD, 15));
 		dob.setBounds(10, 71, 116, 38);
+		dob.setFont(new Font("Arial", Font.BOLD, 15));
 		contentPane.add(dob);
 		
 		JLabel gender = new JLabel("Gender");
-		gender.setFont(new Font("Arial", Font.BOLD, 15));
 		gender.setBounds(10, 105, 116, 38);
+		gender.setFont(new Font("Arial", Font.BOLD, 15));
 		contentPane.add(gender);
 		
 		JLabel phone = new JLabel("Phone Number");
-		phone.setFont(new Font("Arial", Font.BOLD, 15));
 		phone.setBounds(10, 136, 116, 38);
+		phone.setFont(new Font("Arial", Font.BOLD, 15));
 		contentPane.add(phone);
 		
 		JLabel email = new JLabel("E-mail");
-		email.setFont(new Font("Arial", Font.BOLD, 15));
 		email.setBounds(10, 166, 116, 38);
+		email.setFont(new Font("Arial", Font.BOLD, 15));
 		contentPane.add(email);
 		
 		JLabel bloodgrp = new JLabel("Blood Group");
-		bloodgrp.setFont(new Font("Arial", Font.BOLD, 15));
 		bloodgrp.setBounds(10, 200, 116, 38);
+		bloodgrp.setFont(new Font("Arial", Font.BOLD, 15));
 		contentPane.add(bloodgrp);
 		
 		JLabel height = new JLabel("Height");
-		height.setFont(new Font("Arial", Font.BOLD, 15));
 		height.setBounds(10, 239, 116, 38);
+		height.setFont(new Font("Arial", Font.BOLD, 15));
 		contentPane.add(height);
 		
 		JLabel weight = new JLabel("Weight");
-		weight.setFont(new Font("Arial", Font.BOLD, 15));
 		weight.setBounds(10, 278, 116, 38);
+		weight.setFont(new Font("Arial", Font.BOLD, 15));
 		contentPane.add(weight);
 		
 		JLabel profession = new JLabel("Profession");
-		profession.setFont(new Font("Arial", Font.BOLD, 15));
 		profession.setBounds(10, 311, 116, 38);
+		profession.setFont(new Font("Arial", Font.BOLD, 15));
 		contentPane.add(profession);
 		
 		JLabel residence = new JLabel("Residence");
-		residence.setFont(new Font("Arial", Font.BOLD, 15));
 		residence.setBounds(10, 345, 116, 38);
+		residence.setFont(new Font("Arial", Font.BOLD, 15));
 		contentPane.add(residence);
 		
 		JLabel workplace = new JLabel("Work place");
-		workplace.setFont(new Font("Arial", Font.BOLD, 15));
 		workplace.setBounds(10, 381, 116, 38);
+		workplace.setFont(new Font("Arial", Font.BOLD, 15));
 		contentPane.add(workplace);
 		
 		namefeild = new JTextField();
+		namefeild.setBounds(205, 50, 219, 19);
 		namefeild.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 13));
 		namefeild.setBackground(UIManager.getColor("CheckBox.background"));
-		namefeild.setBounds(205, 50, 219, 19);
 		contentPane.add(namefeild);
 		namefeild.setColumns(10);
 		
 		dobfield = new JTextField();
+		dobfield.setBounds(205, 82, 219, 19);
 		dobfield.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 13));
 		dobfield.setColumns(10);
 		dobfield.setBackground(SystemColor.menu);
-		dobfield.setBounds(205, 82, 219, 19);
 		contentPane.add(dobfield);
 		
 		genderfield = new JTextField();
+		genderfield.setBounds(205, 116, 219, 19);
 		genderfield.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 13));
 		genderfield.setColumns(10);
 		genderfield.setBackground(SystemColor.menu);
-		genderfield.setBounds(205, 116, 219, 19);
 		contentPane.add(genderfield);
 		
 		phonefield = new JTextField();
+		phonefield.setBounds(205, 147, 219, 19);
 		phonefield.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 13));
 		phonefield.setColumns(10);
 		phonefield.setBackground(SystemColor.menu);
-		phonefield.setBounds(205, 147, 219, 19);
 		contentPane.add(phonefield);
 		
 		emailfield = new JTextField();
+		emailfield.setBounds(205, 177, 219, 19);
 		emailfield.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 13));
 		emailfield.setColumns(10);
 		emailfield.setBackground(SystemColor.menu);
-		emailfield.setBounds(205, 177, 219, 19);
 		contentPane.add(emailfield);
 		
 		bloodfield = new JTextField();
+		bloodfield.setBounds(205, 211, 219, 19);
 		bloodfield.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 13));
 		bloodfield.setColumns(10);
 		bloodfield.setBackground(SystemColor.menu);
-		bloodfield.setBounds(205, 211, 219, 19);
 		contentPane.add(bloodfield);
 		
 		heightfield = new JTextField();
+		heightfield.setBounds(205, 250, 219, 19);
 		heightfield.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 13));
 		heightfield.setColumns(10);
 		heightfield.setBackground(SystemColor.menu);
-		heightfield.setBounds(205, 250, 219, 19);
 		contentPane.add(heightfield);
 		
 		weightfield = new JTextField();
+		weightfield.setBounds(205, 289, 219, 19);
 		weightfield.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 13));
 		weightfield.setColumns(10);
 		weightfield.setBackground(SystemColor.menu);
-		weightfield.setBounds(205, 289, 219, 19);
 		contentPane.add(weightfield);
 		
 		professionfield = new JTextField();
+		professionfield.setBounds(205, 322, 219, 19);
 		professionfield.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 13));
 		professionfield.setColumns(10);
 		professionfield.setBackground(SystemColor.menu);
-		professionfield.setBounds(205, 322, 219, 19);
 		contentPane.add(professionfield);
 		
 		residencefield = new JTextField();
+		residencefield.setBounds(205, 356, 219, 19);
 		residencefield.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 13));
 		residencefield.setColumns(10);
 		residencefield.setBackground(SystemColor.menu);
-		residencefield.setBounds(205, 356, 219, 19);
 		contentPane.add(residencefield);
 		
 		workfield = new JTextField();
+		workfield.setBounds(205, 392, 219, 19);
 		workfield.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 13));
 		workfield.setColumns(10);
 		workfield.setBackground(SystemColor.menu);
-		workfield.setBounds(205, 392, 219, 19);
 		contentPane.add(workfield);
 		
 		JButton ok1 = new JButton("OK");
+		ok1.setBounds(495, 381, 85, 38);
 		ok1.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
@@ -207,7 +209,7 @@ public class FirstFrame extends JFrame {
 				try 
 				{ 
 					Class.forName("com.mysql.jdbc.Driver");
-					Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/personal_information?characterEncoding=latin1","root","your password"); 
+					Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/personal_information?characterEncoding=latin1","root","Sharanya47*;"); 
 					PreparedStatement ps = conn.prepareStatement("insert into Details(name, dob, gender, ph_no, email, blood_grp, height, weight, profession, residing_area, work_place) values(?,?,?,?,?,?,?,?,?,?,?);");
 					ps.setString(1, namefeild.getText());
  					ps.setString(2, dobfield.getText());
@@ -241,7 +243,6 @@ public class FirstFrame extends JFrame {
 			}
 		});
 		ok1.setFont(new Font("Times New Roman", Font.BOLD, 15));
-		ok1.setBounds(495, 381, 85, 38);
 		contentPane.add(ok1);
 	}
 }
--- Personal_Information_Project/src/Personal_Information_Project/RetrieveDetailsFrame.java
+++ Personal_Information_Project/src/Personal_Information_Project/RetrieveDetailsFrame.java
@@ -6,13 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+
 
 public class RetrieveDetailsFrame extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -35,7 +42,7 @@ public class RetrieveDetailsFrame extends JFrame {
 	 */
 	public RetrieveDetailsFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 678, 490);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -44,7 +51,29 @@ public class RetrieveDetailsFrame extends JFrame {
 		JLabel lblNewLabel = new JLabel("Retrieve Details");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Lucida Sans", Font.BOLD | Font.ITALIC, 17));
-		lblNewLabel.setBounds(10, 11, 414, 21);
+		lblNewLabel.setBounds(71, 10, 414, 21);
 		contentPane.add(lblNewLabel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(39, 49, 556, 350);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setFont(new Font("Arial", Font.BOLD, 13));
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"Name", "Date of Birth", "Gender", "Phone Number", "E-Mail", "Blood Group", "Height", "Weight", "Profession", "Residence", "Work Place"
+			}
+		));
+		
+		JButton fetch = new JButton("Fetch data");
+		fetch.setBounds(269, 411, 102, 32);
+		contentPane.add(fetch);
 	}
 }
+
