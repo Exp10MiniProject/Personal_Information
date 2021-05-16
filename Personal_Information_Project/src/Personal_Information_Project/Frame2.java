e74ff7c2cb50bfcb34220f7ed0cd6667ee69fb2a
Rose Mary <rosemary123c@gmail.com>
2021-05-16 14:15:05 +0530

    Update Frame2.java

--- Personal_Information_Project/src/Personal_Information_Project/Frame2.java
+++ Personal_Information_Project/src/Personal_Information_Project/Frame2.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Frame2 extends JFrame {
 
@@ -35,8 +37,35 @@ public class Frame2 extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel frame2_lable = new JLabel("Enter details");
+		frame2_lable.setFont(new Font("Tahoma", Font.BOLD, 15));
+		frame2_lable.setBounds(22, 11, 130, 28);
+		contentPane.add(frame2_lable);
+	}
+
+	public void NewScreen() 
+	{
+
+		EventQueue.invokeLater(new Runnable()
+		{
+			@Override
+			public void run() 
+			{
+				try
+				{
+					Frame2 frame = new Frame2();
+					frame.setVisible(true);
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+				
+			}
+		});
 	}
 
 }
