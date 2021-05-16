f9b8a1bca939c05712dd6b36de02216f97412ece
Rose Mary <rosemary123c@gmail.com>
2021-05-16 20:17:09 +0530

    comment

--- Personal_Information_Project/src/Personal_Information_Project/FirstFrame.java
+++ Personal_Information_Project/src/Personal_Information_Project/FirstFrame.java
@@ -20,7 +20,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class FirstFrame extends JFrame {
-
+// comment
 	private JPanel contentPane;
 	private JTextField namefeild;
 	private JTextField dobfield;
