b74faa30501a469adecbc75c66081d8c642ef45e
Rose Mary <rosemary123c@gmail.com>
2021-05-16 14:36:21 +0530

    Update FirstFrame.java

--- Personal_Information_Project/src/Personal_Information_Project/FirstFrame.java
+++ Personal_Information_Project/src/Personal_Information_Project/FirstFrame.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class FirstFrame extends JFrame {
 
@@ -35,8 +37,12 @@ public class FirstFrame extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel Enter_label = new JLabel("Enter Details");
+		Enter_label.setFont(new Font("Lucida Sans", Font.BOLD, 16));
+		Enter_label.setBounds(10, 11, 163, 20);
+		contentPane.add(Enter_label);
 	}
-
 }
